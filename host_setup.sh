ba7744e9ce7e360ed34b19ddce4fca7d31000700
Hakam Atassi <univ5905@oscar.ddns.eng.ox.ac.uk>
2025-11-14 14:01:57 +0000

    works

--- host_setup.sh
+++ host_setup.sh
@@ -4,7 +4,11 @@
 # feel free to create any forks of things here...
 
 git clone https://github.com/accel-sim/accel-sim-framework.git shared/accel-sim-framework
+git checkout shared/accel-sim-framework 3c96d32
+
 git clone https://github.com/accel-sim/gpgpu-sim_distribution.git shared/accel-sim-framework/gpu-simulator/gpgpu-sim
+git checkout shared/accel-sim-framework/gpu-simulator/gpgpu-sim b18ee39
+
 # git clone https://github.com/accel-sim/gpu-app-collection.git shared/accel-sim-framework/gpu-app-collection # not needed if you have the official docker. 
 
 
---
+++ shared/init.sh
@@ -0,0 +1,65 @@
+
+ACCELSIM_FRAMEWORK=/root/Repos/shared/accel-sim-framework
+ACCELSIM_DATA=/accel-sim
+export CUDA_INSTALL_PATH=/usr/local/cuda
+export PATH=$CUDA_INSTALL_PATH/bin:$PATH
+export CUDA_VISIBLE_DEVICES=0
+
+
+# install GPGPU #
+cd $ACCELSIM_FRAMEWORK
+pip3 install -r requirements.txt
+source ./gpu-simulator/setup_environment.sh
+make -j12 -C ./gpu-simulator/
+
+
+# install nvbit
+cd $ACCELSIM_FRAMEWORK
+./util/tracer_nvbit/install_nvbit.sh
+make -C ./util/tracer_nvbit/
+
+
+# run example trace
+cd $ACCELSIM_FRAMEWORK/util/tracer_nvbit
+make -C nvbit_release/test-apps
+LD_PRELOAD=./tracer_tool/tracer_tool.so ./nvbit_release/test-apps/vectoradd/vectoradd
+
+
+# make gpgpu workloads (rodinia)
+cd $ACCELSIM_DATA
+source ./gpu-app-collection/src/setup_environment
+make -j -C ./gpu-app-collection/src rodinia_2.0-ft
+make -C ./gpu-app-collection/src data
+
+
+
+
+## YOU ARE NOW SET UP #
+## CLOSE AND RESTART CONTAINER TO PROCEED!! ##
+
+
+ACCELSIM_FRAMEWORK=/root/Repos/shared/accel-sim-framework
+ACCELSIM_DATA=/accel-sim
+export CUDA_INSTALL_PATH=/usr/local/cuda
+export PATH=$CUDA_INSTALL_PATH/bin:$PATH
+export CUDA_VISIBLE_DEVICES=0
+
+source ./gpu-simulator/setup_environment.sh # source (again)
+
+# Run accelsim
+cd $ACCELSIM_FRAMEWORK
+./util/tracer_nvbit/run_hw_trace.py -B rodinia_2.0-ft -D 0 # IDFK what this does
+
+
+# run as SASS (GPGPU)
+./util/job_launching/run_simulations.py -B rodinia_2.0-ft -C RTX3070-SASS -T ./hw_run/traces/device-0/12.8/ -N myTest
+
+# monitor jobs
+./util/job_launching/monitor_func_test.py -v -N myTest
+
+
+# run as PTX (trace based)
+./util/job_launching/run_simulations.py -B rodinia_2.0-ft -C RTX3070-PTX -N myTest-PTX
+
+# Monitor again
+./util/job_launching/monitor_func_test.py -v -N myTest-PTX
