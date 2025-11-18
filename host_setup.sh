1fd376179c9de770ac88766eeceffdcb83954a4c
Hakam Atassi <univ5905@engs-32617.ENG.OX.AC.UK>
2025-11-18 16:20:10 +0000

    update

--- host_setup.sh
+++ host_setup.sh
@@ -2,14 +2,16 @@
 # feel free to create any forks of things here...
 
 
+
+
 # Clone Accel-Sim framework
-git clone https://github.com/accel-sim/accel-sim-framework.git shared/accel-sim-framework
+git clone git@github.com:HakamAtassi/accel-sim-framework.git shared/accel-sim-framework
 cd shared/accel-sim-framework
 git checkout 3c96d32
 cd ../..
 
 # Clone GPGPU-Sim distribution inside the framework
-git clone https://github.com/accel-sim/gpgpu-sim_distribution.git shared/accel-sim-framework/gpu-simulator/gpgpu-sim
+git git@github.com:HakamAtassi/gpgpu-sim_distribution.git hared/accel-sim-framework/gpu-simulator/gpgpu-sim
 cd shared/accel-sim-framework/gpu-simulator/gpgpu-sim
 git checkout b18ee39
 cd ../../../../
